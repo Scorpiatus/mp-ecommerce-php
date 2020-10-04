51cd598462459fb50eabafedd0de1b8e443d4ad8
Scorpiatus <49928168+Scorpiatus@users.noreply.github.com>
2020-10-04 20:36:08 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -11,6 +11,7 @@
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
+    <script src="https://www.mercadopago.com/v2/security.js" view="item"></script>
 
     <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
 
@@ -130,7 +131,76 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    
+                                    <!-- -->
+                                    <?php
+                                        // SDK de Mercado Pago
+                                        require __DIR__ .  '/vendor/autoload.php';
+                                    
+                                        // Agrega credenciales
+                                        MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
+                                        // Crea un objeto de preferencia
+                                        $preference = new MercadoPago\Preference();
+                                        // Metodos de Pago
+                                        $preference->payment_methods = array(
+                                          "excluded_payment_methods" => array(
+                                            array("id" => "amex")
+                                          ),
+                                          "excluded_payment_types" => array(
+                                            array("id" => "atm")
+                                          ),
+                                          "installments" => 6
+                                        );
+                                        // ...
+
+                                        // Datos del Pagador
+                                        $payer = new MercadoPago\Payer();
+                                        $payer->name = "Lalo";
+                                        $payer->surname = "Landa";
+                                        $payer->email = "test_user_63274575@testuser.com";
+                                        $payer->phone = array(
+                                            "area_code" => "11",
+                                            "number" => "12345678"
+                                        );
+                                          
+                                        $payer->address = array(
+                                           "street_name" => "Siempre Viva",
+                                           "street_number" => 742,
+                                           "zip_code" => "8989"
+                                        );
+                                        // ...
+
+                                        //...
+                                        $preference->back_urls = array(
+                                            "success" => "https://scorpiatus-mp-ecommerce-php.herokuapp.com/success.php",
+                                            "failure" => "https://scorpiatus-mp-ecommerce-php.herokuapp.com/failure.php",
+                                            "pending" => "https://scorpiatus-mp-ecommerce-php.herokuapp.com/pending.php"
+                                        );
+                                        $preference->auto_return = "approved";
+                                        // ...
+
+                                        // Crea un ítem en la preferencia
+                                        $item = new MercadoPago\Item();
+                                        $item->id = 1234;
+                                        $item->title = $_POST['title'];
+                                        $item->description = $_POST['title'];
+                                        $item->picture_url = $_POST['img'];
+                                        $item->quantity = 1;
+                                        $item->unit_price = $_POST['price'];
+                                        $preference->items = array($item);
+
+                                        $preference->external_reference = 'hernan@bayresapp.com';
+                                        $preference->notification_url = "https://scorpiatus-mp-ecommerce-php.herokuapp.com/notification.php";
+
+                                        $preference->save();
+
+                                        //Setear el integrator-id
+                                        MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+                                    ?>
+                                                                        
+                                    <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
+
                                 </div>
                             </div>
                         </div>
