af626f7a7901095a4cc0a4a3f8344cbd1ac1c4e3
Scorpiatus <49928168+Scorpiatus@users.noreply.github.com>
2020-10-04 21:27:37 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -138,8 +138,8 @@
                                         require __DIR__ .  '/vendor/autoload.php';
                                     
                                         // Agrega credenciales
+                                        MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
                                         MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-
                                         // Crea un objeto de preferencia
                                         $preference = new MercadoPago\Preference();
                                         // Metodos de Pago
